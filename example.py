17e60a00741373f60eddeba2e7471e685b220d23
khushbt1 <khushbt1@in.ibm.com>
2020-08-02 17:46:52 +0530

    Revert "changes for hairdye"
    This reverts commit d189a066f930a247c64b3c9aa88783198db1c7de.

--- example.py
+++ example.py
@@ -6,7 +6,6 @@ from color_transfer import color_transfer
 import numpy as np
 import argparse
 import cv2
-import os
 
 def show_image(title, image, width = 300):
 	# resize the image to have a constant width, just to
@@ -29,12 +28,10 @@ def str2bool(v):
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-# ap.add_argument("-s", "--source", required = True,
-# 	help = "Path to the source image")
+ap.add_argument("-s", "--source", required = True,
+	help = "Path to the source image")
 ap.add_argument("-t", "--target", required = True,
 	help = "Path to the target image")
-ap.add_argument("-m", "--mask", required = True,
-	help = "Path to the target image")	
 ap.add_argument("-c", "--clip", type = str2bool, default = 't',
 	help = "Should np.clip scale L*a*b* values before final conversion to BGR? "
 		   "Approptiate min-max scaling used if False.")
@@ -43,30 +40,20 @@ ap.add_argument("-p", "--preservePaper", type = str2bool, default = 't',
 ap.add_argument("-o", "--output", help = "Path to the output image (optional)")
 args = vars(ap.parse_args())
 
-# # load the images
-# source = cv2.imread(args["source"])
+# load the images
+source = cv2.imread(args["source"])
 target = cv2.imread(args["target"])
-mask = cv2.imread(args["mask"])
 
-sourcepath = "shades"
+# transfer the color distribution from the source image
+# to the target image
+transfer = color_transfer(source, target, clip=args["clip"], preserve_paper=args["preservePaper"])
 
-for i in os.listdir(sourcepath):
-	print(os.path.join(sourcepath,i))
-	source = cv2.imread(os.path.join(sourcepath,i))
+# check to see if the output image should be saved
+if args["output"] is not None:
+	cv2.imwrite(args["output"], transfer)
 
-	# transfer the color distribution from the source image
-	# to the target image
-	final,transfer,fg = color_transfer(source, target,mask, clip=args["clip"], preserve_paper=args["preservePaper"])
-
-	# check to see if the output image should be saved
-	if args["output"] is not None:
-		cv2.imwrite(args["output"]+i, final)
-		cv2.imwrite("transfer1/"+i,transfer)
-		
-		cv2.imwrite("fg/"+i,fg)
-
-# # show the images and wait for a key press
-# show_image("Source", source)
-# show_image("Target", target)
-# show_image("Transfer", transfer)
-# cv2.waitKey(0)
+# show the images and wait for a key press
+show_image("Source", source)
+show_image("Target", target)
+show_image("Transfer", transfer)
+cv2.waitKey(0)
